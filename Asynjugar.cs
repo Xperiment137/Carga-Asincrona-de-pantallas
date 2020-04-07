a412fb907a952cb65e3f681e97ab46e59031e0d1
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2020-04-08 00:32:27 +0200

    Add files via upload

---
+++ Asynjugar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Asynjugar : MonoBehaviour
+{
+
+    public string Escena;
+    public Button boton1;
+
+    void Start()
+    {
+        boton1.onClick.AddListener(()=>LoadButton());
+
+    }
+
+    void LoadButton()
+    {
+       
+        StartCoroutine(LoadScene());
+    }
+
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+       
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(Escena);
+     
+         
+
+            yield return null;
+        }
+    }
+
